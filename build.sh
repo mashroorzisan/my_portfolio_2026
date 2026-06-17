d7077c2bf6abc906bd6aceed3b2ee537c8742a83
Ishtiaque Ahmed (zisan) <63655171+mashroorzisan@users.noreply.github.com>
2026-06-17 16:27:06 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ npm run build
 
 echo "==> Installing backend dependencies..."
 cd ../backend
-python3 -m venv venv
-./venv/bin/pip install -r requirements.txt
+python3 -m venv /opt/venv
+/opt/venv/bin/pip install -r requirements.txt
 
 echo "==> Build complete."
